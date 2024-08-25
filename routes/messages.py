3c7e3656c702e0af356e80845ba4ff7b583c976e
Daniel Viglione <daniel.viglione@atlanticgenetics.com>
2024-08-25 06:45:37 -0400

    feat: cleanup ingestion

--- routes/messages.py
+++ routes/messages.py
@@ -30,6 +30,8 @@ async def create_message(conversation_id: str, message: MessageModel = Body(...)
     if (
         created_message_id := await Message.create(conversation_id, message)
     ) is not None:
+        # query retrieval and check if conversation has a message with file
+
         return { "_id": created_message_id }
     return {'error': f'Conversation not created'}, 400
 
@@ -76,13 +78,15 @@ async def delete_message(request: Request, conversation_id: str, id: str):
         return deleted_message  
     return { 'error': f'Conversation {id} not found'}, 404
 
+def format_file_for_storage(uuid: str, conversation_id: str, id: str, filename: str):
+    return f'files/{uuid}/conversations/{conversation_id}/messages/{id}/{filename}'
 
 async def upload_file(request: Request, conversation_id: str, id: str, upload_file: UploadFile = File(...)) -> Optional[Dict[str,Any]]:
     uuid = request.state.uuid
     if (
         message_dict := await Message.find(uuid, conversation_id, id)
     ) is not None:
-        path = f'files/{uuid}/conversations/{conversation_id}/messages/{id}/{upload_file.filename}'
+        path = format_file_for_storage(uuid, conversation_id, id, upload_file.filename)
         dir_path = os.path.dirname(path)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
@@ -101,7 +105,8 @@ async def upload_file(request: Request, conversation_id: str, id: str, upload_fi
 async def upload(request: Request, conversation_id: str, id: str, upload_file: UploadFile = File(...), message: Optional[Dict[str,Any]] = Depends(upload_file)):
     if not message:
         return {'error': f'Message {id} not found'}, 404
-    ingestor = DocumentIngestor(f'files/{request.state.uuid}/conversations/{conversation_id}/messages/{id}/{upload_file.filename}', request.state.uuid, conversation_id, id)
+    file = format_file_for_storage(request.state.uuid, conversation_id, id, upload_file.filename)
+    ingestor = DocumentIngestor(file, request.state.uuid, conversation_id, id)
     # TODO: make asynchronous
     embedded_ids = ingestor.ingest()
     logger.logging.warning(f'EMBEDDED IDs: {len(embedded_ids)}')
