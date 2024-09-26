a4c6582939a9391a74049748c16a1e898406defe
DanielViglione <dviglione02@gmail.com>
2024-09-26 00:27:48 -0400

    feat: increase context window and streamline async events

--- orchestrators/chat/llm_models/hf_tgi_llm.py
+++ orchestrators/chat/llm_models/hf_tgi_llm.py
@@ -15,31 +15,10 @@ current_directory = os.path.dirname(current_file_path)
 new_folder_path = os.path.join(current_directory, 'local_tokenizer')
 tokenizer = AutoTokenizer.from_pretrained(new_folder_path)
 
-class InspectionStreamingHandler(StreamingStdOutCallbackHandler):
-    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
-        """Run on new LLM token. Only available when streaming is enabled.
-
-        Args:
-            token (str): The new token.
-            **kwargs (Any): Additional keyword arguments.
-        """
-        logging.warning(f'WHAT IS THE CURRENT TOKEN {token}')
-        sys.stdout.write(token)
-        sys.stdout.flush()
-
-    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
-        """Run when LLM ends running.
-
-        Args:
-            response (LLMResult): The response from the LLM.
-            **kwargs (Any): Additional keyword arguments.
-        """
-        logging.warning(f'WHAT IS THE LLM RESULT {response}')
-
 @dataclass(kw_only=True, slots=True)
 class HFTGI(LLM):
     def __post_init__(self) -> None:
-        streaming_handler = InspectionStreamingHandler()
+        streaming_handler = StreamingStdOutCallbackHandler()
         callbacks = [streaming_handler]
         llm = HuggingFaceEndpoint(
             streaming=True, 
