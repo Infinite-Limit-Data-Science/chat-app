8839131abab05d5988c5e869d0ef844042846657
DanielViglione <dviglione02@gmail.com>
2024-09-30 15:19:14 -0400

    feat: load dashboard

--- models/setting.py
+++ models/setting.py
@@ -8,7 +8,6 @@ from models.mongo_schema import (
     Field,
     PyObjectId
 )
-from bson import ObjectId
 from models.prompt import PromptSchema
 from models.user_model_config import UserModelConfigSchema
 
@@ -34,9 +33,6 @@ class SettingSchema(PrimaryKeyMixinSchema, TimestampMixinSchema):
     class Config:
         populate_by_name = True
         arbitrary_types_allowed = True
-        json_encoders = {
-            ObjectId: str,  # In case there are any ObjectId fields left
-        }
 
 class SettingIdSchema(ChatSchema):
     id: PyObjectId = Field(alias="_id", description='bson object id')
